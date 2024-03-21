0447217235bb22ec37c6a98f9aeaaae03feb754d
Fabio Toniolo Vieira Junior <ftoniolo@viemar.com.br>
2024-03-21 09:37:57 -0300

    v0.2

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="vieutil",  # How you named your package folder (MyLib)
     packages=["vieutil"],  # Chose the same as "name"
-    version="0.1",  # Start with a small number and increase it with every change you make
+    version="0.2",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="Password encryption",  # Give a short description about your library
     author="Fabio Toniolo Vieira Junior",  # Type in your name
     author_email="ftoniolo@viemar.com.br",  # Type in your E-Mail
     url="https://github.com/ftonioloviemar/vieutil",  # Provide either the link to your github or to your website
-    download_url="https://github.com/ftonioloviemar/vieutil/archive/refs/tags/v_0.1.tar.gz",  # I explain this later on
+    download_url="https://github.com/ftonioloviemar/vieutil/archive/refs/tags/V_0.2.tar.gz",  # I explain this later on
     keywords=[
         "viemar",
         "util",
